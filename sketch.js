e6df474f592566d3b5bb73422957570b9198e474
Jacob Burgess <47076545+whoscoffee@users.noreply.github.com>
2021-02-04 19:10:10 -0800

    COLOR

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
 let codes = [];
+let titleColor = [0,0,0];
+let clickCount = 0;
 function setup() {
     createCanvas(windowWidth, windowHeight)
     addLink('/SnakeGame/index.html')
@@ -19,20 +21,31 @@ function addLink(str){
     for(i = 0; i < index; i++) subStr[i] = str[1+i]//creates a substring of directory name
     codes[codes.length] = createA(str, join(subStr, ''), 'Blank')//makes the element
 }
+
 //helper function for draw
 function redRaw(){
-    background(255,255,255)
-    fill(0)
-    textSize(windowWidth*0.05)
+    background(255-titleColor[0] ,255-titleColor[1],255-titleColor[2])
+    let titleSize = height*0.05
+    fill(titleColor)
+    textSize(titleSize)
     textAlign(CENTER,TOP)
-    text("WhosCoffee", 0, 0, windowWidth)
-    let heightValue = height/(codes.length+1)
+    text("WhosCoffee", 0, 0, width)
+    let heightValue = (height-titleSize)/(codes.length)
     for(var i = 0; i < codes.length; i++){
         codes[i].position(width/2,heightValue+i*heightValue)
         codes[i].center('horizontal')
         codes[i].style('text-decoration', 'none')
-        codes[i].style('color', color(0))//css is basically cancer,change my mind
+        codes[i].style('color', color(titleColor))//css is basically cancer,change my mind
     }
+}
+function draw(){
+    titleColor[0] = map(mouseX,0,width,0,255)
+    titleColor[1] = map(mouseY,0,height,0,255)
+    titleColor[2] = (clickCount*10)%255
+    redRaw();
+}
+function mouseClicked(){
+    clickCount++;
 }
 function windowResized(){
     resizeCanvas(windowWidth,windowHeight)
